4426d546eba6db61bed717b8eb729176f4085b78
Thiago Fernandes Dornelles <thiago.fdornelles@gmail.com>
2021-05-06 23:43:40 -0300

    tailwind and framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/tailwind.css'
-import '../styles/globals.css'
+import { AnimateSharedLayout } from "framer-motion";
+
+import "../styles/tailwind.css";
+import "../styles/globals.css";
+
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnimateSharedLayout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimateSharedLayout>
+  );
 }
 
-export default MyApp
+export default MyApp;
